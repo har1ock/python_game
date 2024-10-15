e30b13a40d5b34ca26012b56709d6c1f1e362e8b
har1ok <xarlokua@gmail.com>
2024-10-15 18:00:04 +0300

    change 1 change 2

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,18 +1,18 @@
 import sys
 import pygame
 
+from settings import Settings
 
 class AlienInvasion:
     """Загальний клас, що керує ресурсами та поведінкою гри."""
     def __init__(self):
         """Ініціалізувати гру, створити ресурси гри"""
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
-
-        # Задати колір фону
-        self.bg.color = (230, 230, 230)
 
     def run_game(self):
         """Розпочати головний цикл гри."""
@@ -21,6 +21,9 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+
+            # Наново перемалювати екран на кожній ітерації циклу
+            self.screen.fill(self.settings.bg_color)
 
             # Показати останій намальований екран.
             pygame.display.flip()
