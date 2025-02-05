e800943aacc5fac4e604f153a8088d308d2750a5
harlock <xarlokua@gmail.com>
2025-02-05 16:50:36 +0200

    test1

--- ship.py
+++ ship.py
@@ -28,9 +28,9 @@ class Ship:
         індикатора руху.
         """
         #Оновити значення ship.x, а не rect.
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
 
         #Оновити об'єкт rect з self.x.
